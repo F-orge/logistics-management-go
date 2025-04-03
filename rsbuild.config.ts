09f7d1d6612d72f48cf7ea3f419e9ea2b1fd6e11
karlrobeck <karlalferezfx@gmail.com>
2025-04-03 20:19:34 +0800

    add proxy config to rsbuild

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,20 +4,25 @@ import { pluginSolid } from "@rsbuild/plugin-solid";
 import path from "node:path";
 
 export default defineConfig({
-  plugins: [
-    pluginBabel({
-      include: /\.(?:jsx|tsx)$/,
-    }),
-    pluginSolid(),
-  ],
-  source: {
-    entry: {
-      index: "./web/index.tsx",
-    },
-  },
-  resolve: {
-    alias: {
-      "~": path.resolve(import.meta.dirname, "./web"),
-    },
-  },
+	plugins: [
+		pluginBabel({
+			include: /\.(?:jsx|tsx)$/,
+		}),
+		pluginSolid(),
+	],
+	source: {
+		entry: {
+			index: "./web/index.tsx",
+		},
+	},
+	server: {
+		proxy: {
+			"/api": "http://localhost:8090/api",
+		},
+	},
+	resolve: {
+		alias: {
+			"~": path.resolve(import.meta.dirname, "./web"),
+		},
+	},
 });
