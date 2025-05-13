51ff3c304f5790b2b4d01d1e591b983b424ec310
mhellb10 <bergonio.mhell101001@gmail.com>
2025-05-13 16:15:22 +0800

    add fields and constraints

---
+++ migrations/1747123628_updated_orders.go
@@ -0,0 +1,63 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3527180448")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE UNIQUE INDEX ` + "`" + `idx_FFKluND6eN` + "`" + ` ON ` + "`" + `orders` + "`" + ` ('order_id_custom')"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text1865734286",
+			"max": 0,
+			"min": 0,
+			"name": "order_id_custom",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": true,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3527180448")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text1865734286")
+
+		return app.Save(collection)
+	})
+}
