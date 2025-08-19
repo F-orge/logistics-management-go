edac4b0737de4c20fa002b8c02b97103464b2340
Karl robeck alferez <karlalferezfx@gmail.com>
2025-08-19 15:34:59 +0800

    feat: update package version to 1.0.3 and add migration for updated LMS addresses collection

---
+++ migrations/1755588879_updated_lms_addresses.go
@@ -0,0 +1,66 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1659436351")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("geoPoint1092145443")
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"hidden": false,
+			"id": "geoPoint2246143851",
+			"name": "coordinates",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "geoPoint"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1659436351")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(9, []byte(`{
+			"hidden": false,
+			"id": "geoPoint1092145443",
+			"name": "latitude",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "geoPoint"
+		}`)); err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"hidden": false,
+			"id": "geoPoint2246143851",
+			"name": "longitude",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "geoPoint"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
