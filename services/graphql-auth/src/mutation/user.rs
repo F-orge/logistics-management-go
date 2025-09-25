ffad919e9602be24c7be6115bacc833ba348a0ec
Karl robeck alferez <karlalferezfx@gmail.com>
2025-09-25 16:42:42 +0800

    feat: implement sign-in functionality with email and password validation

--- services/graphql-auth/src/mutation/user.rs
+++ services/graphql-auth/src/mutation/user.rs
@@ -1,6 +1,9 @@
 use async_graphql::{Context, InputObject, Object, SimpleObject};
+use chrono::{Duration, Utc};
+use sqlx::PgPool;
+use uuid::Uuid;
 
-use crate::models::user;
+use crate::models::{session, user};
 
 #[derive(Debug, Clone, InputObject)]
 pub struct SignInEmailInput {
@@ -24,6 +27,36 @@ impl Mutation {
         ctx: &Context<'_>,
         payload: SignInEmailInput,
     ) -> async_graphql::Result<SignInResponse> {
-        todo!("Sign in")
+        // todo: make this hashable password
+
+        let db = ctx.data::<PgPool>()?;
+
+        // make sure the user isn't sign in
+        if ctx.data_opt::<user::Model>().is_some() || ctx.data_opt::<session::Model>().is_some() {
+            return Err(async_graphql::Error::new("User already signed in"));
+        }
+
+        let user = sqlx::query_as::<_, user::Model>(
+            r#"select auth.user.* from auth.user inner join auth.account on auth.user.id = auth.account_user.id where auth.user.email = ? and auth.account.password = ?"#,
+        )
+        .bind(payload.email)
+        .bind(payload.password)
+        .fetch_optional(db)
+        .await?
+        .ok_or(async_graphql::Error::new("Invalid email or password"))?;
+
+        let new_session = sqlx::query_as::<_, session::Model>(
+            "insert into auth.session (token,expires_at,user_id) values (?,?,?) returning *",
+        )
+        .bind(Uuid::new_v4())
+        .bind(Utc::now() + Duration::seconds(3600))
+        .bind(user.id)
+        .fetch_one(db)
+        .await?;
+
+        Ok(SignInResponse {
+            token: new_session.token,
+            user,
+        })
     }
 }
