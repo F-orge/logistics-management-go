9a9aca0ba21ba6349968bba995d412d31971e0f7
Karl robeck alferez <karlalferezfx@gmail.com>
2025-10-02 09:39:22 +0800

    feat: implement API route for authentication with GET and POST handlers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,16 @@
-import { betterAuth } from 'better-auth';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { admin, bearer } from 'better-auth/plugins';
-import { db } from '@/db';
-import * as betterAuthSchema from '../db/schemas/better-auth.sql';
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { admin, bearer } from "better-auth/plugins";
+import { db } from "@/db";
 
 export const authFactory = (dbClient: typeof db) =>
   betterAuth({
-    database: drizzleAdapter(dbClient, {
-      provider: 'pg',
-      schema: {
-        ...betterAuthSchema,
-      },
-    }),
+    // database: drizzleAdapter(dbClient, {
+    //   provider: 'pg',
+    //   schema: {
+    //     ...betterAuthSchema,
+    //   },
+    // }),
     emailAndPassword: {
       enabled: true,
     },
