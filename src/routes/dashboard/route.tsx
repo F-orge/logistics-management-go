8d389a42d21faa33065ef5e5137baa16e184289c
Karl robeck alferez <karlalferezfx@gmail.com>
2025-10-04 02:09:14 +0800

    feat: implement SystemBreadcrumbs component and replace static breadcrumbs in RouteComponent

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -1,12 +1,5 @@
 import { AppSidebar } from '@/components/app-sidebar';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
+import { SystemBreadcrumbs } from '@/components/system-breadcrumbs';
 import { Separator } from '@/components/ui/separator';
 import {
   SidebarInset,
@@ -31,19 +24,7 @@ function RouteComponent() {
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
             />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <SystemBreadcrumbs />
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
