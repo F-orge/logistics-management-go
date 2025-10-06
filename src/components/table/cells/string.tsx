6593e77c1440d02667417a8c0b6491fdd85f3c5a
Karl robeck alferez <karlalferezfx@gmail.com>
2025-10-07 04:17:17 +0800

    feat(table): add StringCell component with editable input and tooltip support

---
+++ src/components/table/cells/string.tsx
@@ -0,0 +1,44 @@
+import { Field, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import React from 'react';
+
+export type StringCellProps = {
+  value: string;
+  onSave?: (value: string) => Promise<unknown> | unknown;
+  length?: number; /// number of characters to show. if exceed convert to tooltip
+};
+
+const StringCell = (props: StringCellProps) => {
+  const [edit, setEdit] = React.useState(false);
+  const [value, setValue] = React.useState<string>(props.value);
+
+  return (
+    <Field>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <FieldLabel onDoubleClick={() => setEdit(true)}>{value}</FieldLabel>
+        </TooltipTrigger>
+        {value.length > (props.length || 0) && (
+          <TooltipContent>{value}</TooltipContent>
+        )}
+      </Tooltip>
+      {edit && (
+        <Input
+          onSubmit={() => {
+            props.onSave?.(value);
+            setEdit(false);
+          }}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      )}
+    </Field>
+  );
+};
+
+export default StringCell;
