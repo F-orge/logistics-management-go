5bdcb44f4f4bbc467ecd814f77ec8f094dc1ef08
Karl robeck alferez <karlalferezfx@gmail.com>
2025-10-16 06:55:21 +0800

    feat: implement frontend structure with routing, theming, and ORPC integration; add global styles and configuration

---
+++ apps/frontend/rsbuild.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import tailwindcss from '@tailwindcss/postcss';
+import { tanstackRouter } from '@tanstack/router-plugin/rspack';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  source: {
+    entry: {
+      index: './src/index.tsx',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+      '@packages/ui': '../../packages/ui',
+      '@pacakage/db': '../../packages/db',
+      '@packages/rpc': '../../packages/rpc'
+    },
+  },
+  tools: {
+    postcss: {
+      postcssOptions: {
+        plugins: [tailwindcss],
+      },
+    },
+    rspack: {
+      plugins: [
+        tanstackRouter({
+          target: 'react',
+          autoCodeSplitting: true,
+        }),
+      ],
+    },
+  },
+  output: {
+    distPath: {
+      root: '.output/frontend',
+    },
+  },
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+});
---
+++ apps/frontend/src/lib/orpc.ts
@@ -0,0 +1,22 @@
+import { createORPCClient, onError } from '@orpc/client'
+import { RPCLink } from '@orpc/client/fetch'
+import { BatchLinkPlugin } from '@orpc/client/plugins'
+import type { InferRouterInputs, InferRouterOutputs, RouterClient } from '@orpc/server'
+import type * as orpcRouter from '@packages/rpc/src/handlers/index'
+
+// orpc integration
+const link = new RPCLink({
+  url: `${window.location.origin}/api/orpc`,
+  interceptors: [
+    onError((error) => {
+      console.error(error)
+    }),
+  ],
+  plugins: [
+    new BatchLinkPlugin({
+      groups: [{ condition: (option) => true, context: {} }],
+    }),
+  ],
+})
+
+export const orpcClient: RouterClient<typeof orpcRouter> = createORPCClient(link)
