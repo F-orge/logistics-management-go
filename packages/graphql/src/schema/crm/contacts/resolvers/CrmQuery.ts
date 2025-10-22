540df1cf31f7b195f7c69febd4fd5bbc36210714
Karl robeck alferez <karlalferezfx@gmail.com>
2025-10-23 06:40:15 +0800

    feat: Implement CRUD operations for cases, contacts, and interactions in GraphQL resolvers

--- packages/graphql/src/schema/crm/contacts/resolvers/CrmQuery.ts
+++ packages/graphql/src/schema/crm/contacts/resolvers/CrmQuery.ts
@@ -1,4 +1,25 @@
-import type   { CrmQueryResolvers } from './../../../types.generated';
-    export const CrmQuery: Pick<CrmQueryResolvers, 'contact'|'contacts'> = {
-    /* Implement CrmQuery resolver logic here */
-  };
+import { Contacts } from "../../../../zod.schema";
+import type { CrmQueryResolvers } from "./../../../types.generated";
+export const CrmQuery: Pick<CrmQueryResolvers, "contact" | "contacts"> = {
+  contacts: async (_parent, args, ctx) => {
+    let query = ctx.db.selectFrom("crm.contacts").selectAll();
+
+    if (args.page && args.perPage) {
+      const offset = (args.page - 1) * args.perPage;
+      query = query.offset(offset).limit(args.perPage);
+    }
+
+    const results = await query.execute();
+
+    return results as unknown as Contacts[];
+  },
+  contact: async (_parent, args, ctx) => {
+    const result = await ctx.db
+      .selectFrom("crm.contacts")
+      .selectAll()
+      .where("id", "=", args.id)
+      .executeTakeFirstOrThrow();
+
+    return result as unknown as Contacts;
+  },
+};
--- packages/graphql/src/schema/crm/interactions/resolvers/CrmQuery.ts
+++ packages/graphql/src/schema/crm/interactions/resolvers/CrmQuery.ts
@@ -1,4 +1,26 @@
-import type   { CrmQueryResolvers } from './../../../types.generated';
-    export const CrmQuery: Pick<CrmQueryResolvers, 'interaction'|'interactions'> = {
-    /* Implement CrmQuery resolver logic here */
+import { Interactions } from "../../../../zod.schema";
+import type { CrmQueryResolvers } from "./../../../types.generated";
+export const CrmQuery: Pick<CrmQueryResolvers, "interaction" | "interactions"> =
+  {
+    interactions: async (_parent, args, ctx) => {
+      let query = ctx.db.selectFrom("crm.interactions").selectAll();
+
+      if (args.page && args.perPage) {
+        const offset = (args.page - 1) * args.perPage;
+        query = query.offset(offset).limit(args.perPage);
+      }
+
+      const results = await query.execute();
+
+      return results as unknown as Interactions[];
+    },
+    interaction: async (_parent, args, ctx) => {
+      const result = await ctx.db
+        .selectFrom("crm.interactions")
+        .selectAll()
+        .where("id", "=", args.id)
+        .executeTakeFirstOrThrow();
+
+      return result as unknown as Interactions;
+    },
   };
