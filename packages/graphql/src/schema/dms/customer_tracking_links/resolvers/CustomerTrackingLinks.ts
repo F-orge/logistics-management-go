4904b212ac48b7051d4bc8c1452d8641ea69dc89
Karl robeck alferez <karlalferezfx@gmail.com>
2025-10-24 01:34:58 +0800

    feat(graphql): implement resolvers for CustomerTrackingLinks, DeliveryRoutes, DeliveryTasks, DriverLocations, DmsProofOfDeliveries, and TaskEvents

--- packages/graphql/src/schema/dms/customer_tracking_links/resolvers/CustomerTrackingLinks.ts
+++ packages/graphql/src/schema/dms/customer_tracking_links/resolvers/CustomerTrackingLinks.ts
@@ -1,4 +1,18 @@
-import type   { CustomerTrackingLinksResolvers } from './../../../types.generated';
-    export const CustomerTrackingLinks: CustomerTrackingLinksResolvers = {
-    /* Implement CustomerTrackingLinks resolver logic here */
-  };
+import { DeliveryTasks } from "../../../../zod.schema";
+import type { CustomerTrackingLinksResolvers } from "./../../../types.generated";
+export const CustomerTrackingLinks: CustomerTrackingLinksResolvers = {
+  deliveryTask: async (parent, _args, ctx) => {
+    const result = await ctx.db
+      .selectFrom("dms.deliveryTasks")
+      .selectAll("dms.deliveryTasks")
+      .innerJoin(
+        "dms.customerTrackingLinks",
+        "dms.customerTrackingLinks.deliveryTaskId",
+        "dms.deliveryTasks.id"
+      )
+      .where("dms.customerTrackingLinks.id", "=", parent.id as string)
+      .executeTakeFirstOrThrow();
+
+    return result as unknown as DeliveryTasks;
+  },
+};
