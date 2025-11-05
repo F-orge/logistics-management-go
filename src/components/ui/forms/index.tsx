8f837d235fd23be290333f3c78d1c6774cf510b5
Karl robeck alferez <karlalferezfx@gmail.com>
2025-11-06 02:31:24 +0800

    feat(dialog): implement FormDialog component for enhanced form handling and user interaction

--- src/components/ui/forms/index.tsx
+++ src/components/ui/forms/index.tsx
@@ -1,11 +1,45 @@
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+import FormDialog from "./utils/dialog";
+import {
+  BoolField,
+  CheckboxGroupField,
+  DateTimeField,
+  EmailField,
+  FileField,
+  GeoPointField,
+  JSONField,
+  NumberField,
+  RadioGroupField,
+  RelationField,
+  RichEditorField,
+  SelectField,
+  TextareaField,
+  TextField,
+  URLField,
+} from "./fields";
 
 export const { fieldContext, formContext, useFieldContext, useFormContext } =
-	createFormHookContexts();
+  createFormHookContexts();
 
 export const { useAppForm, withFieldGroup, withForm } = createFormHook({
-	fieldContext,
-	formContext,
-	fieldComponents: {},
-	formComponents: {},
+  fieldContext,
+  formContext,
+  fieldComponents: {
+    BoolField,
+    CheckboxGroupField,
+    DateTimeField,
+    EmailField,
+    FileField,
+    GeoPointField,
+    JSONField,
+    NumberField,
+    RadioGroupField,
+    RelationField,
+    RichEditorField,
+    SelectField,
+    TextField,
+    TextareaField,
+    URLField,
+  },
+  formComponents: { FormDialog },
 });
