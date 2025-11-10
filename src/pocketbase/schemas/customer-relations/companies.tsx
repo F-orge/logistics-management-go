2180ddc817deae231d0be29a09a73ba3ee59dac7
Karl robeck alferez <karlalferezfx@gmail.com>
2025-11-10 21:31:04 +0800

    refactor: Simplify CompanySchema by removing unused imports and field configuration

--- src/pocketbase/schemas/customer-relations/companies.tsx
+++ src/pocketbase/schemas/customer-relations/companies.tsx
@@ -1,15 +1,5 @@
-import React from "react";
 import z from "zod";
-import { fieldConfigFactory } from "@/components/ui/autoform";
 
 export const CompanySchema = z.object({
-  name: z
-    .string()
-    .min(1)
-    .max(255)
-    .check(
-      fieldConfigFactory<"boolean">()({
-        fieldType: "boolean",
-      })
-    ),
+  name: z.string().min(1).max(255),
 });
