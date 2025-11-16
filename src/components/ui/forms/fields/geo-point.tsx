6db9739e5747f4616289b55b4d337f35b303d59e
Karl robeck alferez <karlalferezfx@gmail.com>
2025-11-17 04:03:29 +0800

    feat(delivery-management): implement driver location CRUD actions with forms for create, update, and delete functionalities
    - Added DeleteDriverLocation component for deleting driver locations.
    - Created DriverLocationActions component to manage the display of create, update, and delete forms based on search query actions.
    - Implemented UpdateDriverLocationForm component for updating driver location details.
    - Introduced a comprehensive Map component with various drawing and interaction capabilities, including markers, shapes, and layer management.

--- src/components/ui/forms/fields/geo-point.tsx
+++ src/components/ui/forms/fields/geo-point.tsx
@@ -1,6 +1,18 @@
-import type React from "react";
+import { LatLngExpression } from "leaflet";
+import React, { useEffect, useId } from "react";
 import { Field, FieldDescription, FieldError, FieldLabel } from "../../field";
 import { Input } from "../../input";
+import {
+  Map,
+  MapDrawControl,
+  MapDrawDelete,
+  MapDrawEdit,
+  MapDrawMarker,
+  MapDrawUndo,
+  MapMarker,
+  MapTileLayer,
+  useLeaflet,
+} from "../../map";
 import { useFieldContext } from "..";
 import { TooltipFieldLabel } from "../utils/tooltip-field-label";
 
@@ -9,44 +21,61 @@ export type GeoPointFieldProps = {};
 type GeoPoint = { lat: number; lng: number };
 
 const GeoPointField = (props: GeoPointFieldProps) => {
-	const field = useFieldContext<GeoPoint>();
+  const field = useFieldContext<GeoPoint>();
+  const [currentPosition, setCurrentPosition] = React.useState<GeoPoint | null>(
+    null
+  );
+  const { L } = useLeaflet();
 
-	return (
-		<div className="flex gap-2">
-			<Input
-				value={field.state.value?.lat ?? ""}
-				onChange={(e) => {
-					const lat = parseFloat(e.target.value);
-					if (!isNaN(lat)) {
-						field.handleChange({
-							lat,
-							lng: field.state.value?.lng ?? 0,
-						});
-					}
-				}}
-				placeholder="Latitude"
-				type="number"
-				step="0.00001"
-				disabled
-			/>
-			<Input
-				value={field.state.value?.lng ?? ""}
-				onChange={(e) => {
-					const lng = parseFloat(e.target.value);
-					if (!isNaN(lng)) {
-						field.handleChange({
-							lat: field.state.value?.lat ?? 0,
-							lng,
-						});
-					}
-				}}
-				placeholder="Longitude"
-				type="number"
-				step="0.00001"
-				disabled
-			/>
-		</div>
-	);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const geoPoint = [
+        position.coords.latitude,
+        position.coords.longitude,
+      ] as LatLngExpression;
+      setCurrentPosition({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+      if (!field.state.value) {
+        field.setValue({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      }
+    });
+  }, [field]);
+
+  const center = currentPosition;
+
+  if (!center) {
+    return <div>Loading map...</div>;
+  }
+
+  return (
+    <Map id={field.name} center={field.state.value ?? [center.lat, center.lng]}>
+      <MapTileLayer
+        name={field.name}
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+      />
+      {field.state.value && <MapMarker position={field.state.value} />}
+      <MapDrawControl
+        onLayersChange={(layers) => {
+          layers.eachLayer((layer) => {
+            if (layer instanceof L!.Marker) {
+              const latLng = layer.getLatLng();
+              field.setValue({ lat: latLng.lat, lng: latLng.lng });
+            }
+          });
+        }}
+      >
+        <MapDrawMarker />
+        <MapDrawEdit />
+        <MapDrawDelete />
+        <MapDrawUndo />
+      </MapDrawControl>
+    </Map>
+  );
 };
 
 export default GeoPointField;
